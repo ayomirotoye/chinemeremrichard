b4a788521bfb7eaf37b1365369e164899de9a212
ichie chinemrem richard <chinemeremichie@gmail.com>
2023-04-17 01:07:13 +0100

    formatted file

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const db = require("./db");
 
 const app = express();
 const PORT = 5000;
 
-
-
-/** Decode Form URL Encoded data */
-app.use(express.urlencoded());
+app.use(bodyParser.json());
 
 /** Show page with  task */
 app.get("/", (req, res) => {
-  res.send('Hello AVVIC');
+  res.send("Hello AVVIC");
 });
-
 
 /** Run the app */
 
 // Connecting to database using mongoose library
-db.connectToMongoDB().then(()=>{
-    // Start Listenting for the server on PORT
-app.listen(PORT, () =>
-success({
-  message: `Server successfully started on http://localhost:${PORT}`,
-})
-);
-})
+db.connectToMongoDB().then(() => {
+  // Start Listenting for the server on PORT
+  app.listen(PORT, () =>
+    success({
+      message: `Server successfully started on http://localhost:${PORT}`,
+    })
+  );
+});
